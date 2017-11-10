9e8166005c38e2738d2a0d761feba06114d853ee
Kostas <korekostas@hotmail.com>
2017-11-10 19:25:16 +0100

    changes css install npm add classes html

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvar = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+
+gulp.task('default', function(){
+  console.log("Created a gulp task");
+});
+
+gulp.task('html', function(){
+  console.log("Imagine Dragons");
+});
+
+gulp.task('styles', function(){
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvar, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function(){
+
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/**.css', function(){
+    gulp.start('styles');
+  });
+
+}); //task watch
